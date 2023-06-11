54451bc4fb514e36231da05816f6a3b748d36cb3
Walter <walterlaicollege@gmail.com>
2023-06-11 08:18:20 -0400

    converted faces to proper shape

--- image_processing.py
+++ image_processing.py
@@ -1,8 +1,23 @@
 import numpy as np
-
+import cv2
 
 def convert_to_greyscale(img):
     #the algorithm to convert a RGB to greyscale described in: https://stackoverflow.com/a/51286918
     color_weights = np.array([0.07, 0.72, 0.21])
     grey_image = np.tensordot(a=img, b=color_weights, axes=(2,0))
     return grey_image
+
+cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+def extract_face(gray):
+    #Taken from
+    #https://www.tutorialspoint.com/how-to-detect-cat-faces-in-an-image-in-opencv-using-python#:~:text=Steps%201%20Import%20the%20required%20library.%20...%202,the%20original%20image%20using%20cv2.rectangle%20%28%29.%20More%20items
+
+    # Detects faces in the input image
+    faces = cascade.detectMultiScale(gray)
+    # if atleast one face id detected
+    if len(faces) > 0:
+        x,y,w,h = faces[0]
+        return gray[y:y+h,x:x+w]
+    else:
+        raise ValueError("No cat face detected")
---
+++ train_model.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+from image_processing import convert_to_greyscale, extract_face
+import os
+import re
+import matplotlib.pyplot as plt
+import time
+
+data_folder      = 'lfw_funneled'
+data_subfolders = os.listdir(data_folder)
+picture_paths    = [[f'{data_folder}/{subfolder}/{path}' for path in os.listdir(data_folder+'/'+subfolder)][0] for subfolder in data_subfolders
+                    if re.findall(pattern='^[_a-zA-Z]+$', string=subfolder)]
+rng = np.random.RandomState(seed=1)
+n_samples = 100
+sampling_index = rng.randint(low = 0, high = len(picture_paths), size = n_samples)
+picture_length = 250
+picture_width  = 250
+
+#flattened_pictures = np.zeros(shape=(picture_length*picture_width, n_samples))
+face_shapes    = np.zeros(shape=(n_samples,2))
+pic_size       = (116,116)
+flat_face_list = []
+
+start = time.time()
+for face_number, sample_number in enumerate(sampling_index):
+    pic_array = cv2.imread(picture_paths[sample_number])
+    greyscale_array = convert_to_greyscale(pic_array)
+    greyscale_array = np.uint8(greyscale_array)
+    try:
+        zoom_in_face     =  extract_face(greyscale_array)
+    except ValueError: #if no face is present
+        continue
+    resized_face = cv2.resize(src = zoom_in_face, dsize=pic_size)
+    flat_face_list.append(resized_face.flatten())
+    #flattened_pictures[:,column_number] = greyscale_array.flatten()
+print(time.time()-start)
+
+flat_faces = np.array(flat_face_list).T
+#Check that 3rd column is a picture plt.imshow(flat_faces[:,3].reshape(116,116));plt.show()
+
+average_face = flat_faces.mean(axis=1)[:,np.newaxis]
+centered_faces = flat_faces - average_face
+normalized_faces = centered_faces/np.sum(centered_faces**2,axis=0)**0.5
+
+print()
+
